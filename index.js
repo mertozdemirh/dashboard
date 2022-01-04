e8a509ceb05dd0eefb03fcbf92b2f9f45441bf9e
mert özdemir <mertozdemir91@hotmail.com>
2022-01-04 21:06:40 +0300

    time

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ fetch("https://api.coingecko.com/api/v3/coins/ethereum")
         `
     })
     .catch(err => console.error(err))
+
+function getCurrentTime() {
+        const date = new Date()
+        document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+    
+setInterval(getCurrentTime, 1000)
+
